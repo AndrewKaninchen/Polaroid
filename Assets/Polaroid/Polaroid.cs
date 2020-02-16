58a1253384ace5e9af5e45f6da8995550c934b0b
André Luiz de Andrade Coelho <andre.luiz.coelho@outlook.com>
2020-02-16 01:10:38 -0300

    Getting close? Dealing with some border cases. Some are still missing.

--- Assets/Polaroid/Polaroid.cs
+++ Assets/Polaroid/Polaroid.cs
@@ -7,6 +7,7 @@ using UnityEngine.Experimental.Rendering;
 using UnityEngine.Rendering.UI;
 using UnityEngine.Serialization;
 
+
 public class Polaroid : MonoBehaviour
 {
 	private Transform parentCamera;
@@ -57,14 +58,14 @@ public class Polaroid : MonoBehaviour
 			Gizmos.color = c;
 		}
 		
-//		foreach (var point in projectedPointsToDraw3)
-//		{
-//			var c = Gizmos.color;
-//			var inside = IsInsideFrustum(point, GeometryUtility.CalculateFrustumPlanes(cam));
-//			Gizmos.color = Color.magenta;
-//			Gizmos.DrawWireCube(point, Vector3.one * 0.05f);
-//			Gizmos.color = c;
-//		}
+		foreach (var point in projectedPointsToDraw3)
+		{
+			var c = Gizmos.color;
+			var inside = IsInsideFrustum(point, GeometryUtility.CalculateFrustumPlanes(cam));
+			Gizmos.color = Color.magenta;
+			Gizmos.DrawWireCube(point, Vector3.one * 0.05f);
+			Gizmos.color = c;
+		}
 	}
 
 	private void Start()
@@ -75,25 +76,26 @@ public class Polaroid : MonoBehaviour
 		pictureMaterial = picture.GetComponent<Renderer>().material;
 	}
 	
-	private void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-//			if (frame.activeSelf)
+//	private void Update()
+//	{
+//		{
+//			var planes = GeometryUtility.CalculateFrustumPlanes(cam);
+//			for (var i = 0; i < planes.Length; i++)
 //			{
-//				if (!snapShotSaved)
+//				var plane0 = planes[i];
+//				for (var j = i+1; j < planes.Length; j++)
 //				{
-//					Snapshot();
-//				}
-//				else
-//				{
-//					Place();
+//					var plane1 = planes[j];
+//					MathExtensions.PlanePlaneIntersection(out var linePoint, out var lineVec,
+//						plane0.normal, plane0.normal * plane0.distance,
+//						plane1.normal, plane1.normal * plane1.distance);
+//
+//					Debug.DrawLine(linePoint, lineVec,
+//						Color.red);
 //				}
 //			}
-//			else
-//				frame.SetActive(true);
-		}
-	}
+//		}
+//	}
 
 	public void Place()
 	{
@@ -102,7 +104,7 @@ public class Polaroid : MonoBehaviour
 		
 		foreach (var obj in toBePlaced)
 		{
-			print(obj.ToString());
+//			print(obj.ToString());
 			obj.SetActive(true);
 			obj.transform.SetParent(null);
 		}
@@ -122,20 +124,20 @@ public class Polaroid : MonoBehaviour
 	     var staticObjects = GameObject.FindGameObjectsWithTag("StaticObject");
 	     var dynamicObjects = GameObject.FindGameObjectsWithTag("DynamicObject");
 	     
-	     foreach (var staticObject in staticObjects)
+	     foreach (var obj in staticObjects)
 	     {
-	         var mesh = staticObject.GetComponent<MeshFilter>().mesh;
-	         //if (!GeometryUtility.TestPlanesAABB(planes, mesh.bounds)) continue;
+	         var mesh = obj.GetComponent<MeshFilter>().mesh;
+//	         if (!GeometryUtility.TestPlanesAABB(planes, mesh.bounds)) continue;
 	         
 	         var meshVertices = mesh.vertices.ToList();
 	         var meshTriangles = mesh.triangles;
 
-	         var matchingVertices = GetVerticesInsideViewFrustum(staticObject, in meshVertices, planes);
+	         var matchingVertices = GetVerticesInsideViewFrustum(obj.transform, in meshVertices, planes);
 	         if (matchingVertices.Count == 0) continue;
 	         
-	         var newTriangles = GetTrianglesInsideViewFrustrum(staticObject.transform, ref meshVertices, ref matchingVertices, in meshTriangles, in planes);
+	         var newTriangles = GetTrianglesInsideViewFrustrum(obj.transform, ref meshVertices, ref matchingVertices, in meshTriangles, in planes);
 			 RemapVerticesAndTrianglesToNewMesh(in matchingVertices, in meshVertices, ref newTriangles, out var newVertices);
-			 PrepareCreateNewObject(staticObject, newVertices, newTriangles);
+			 PrepareCreateNewObject(obj, newVertices, newTriangles);
 	     }
 	     
 	     snapShotSaved = true;
@@ -149,28 +151,28 @@ public class Polaroid : MonoBehaviour
 		return planes.All(plane => plane.GetSide(point));
 	}
 
-	private List<int> GetVerticesInRelationToViewFrustum(GameObject obj, in List<Vector3> meshVertices, Plane[] planes, bool inside)
+	private List<int> GetVerticesInRelationToViewFrustum(Transform objTransform, in List<Vector3> meshVertices, Plane[] planes, bool inside)
 	{
 		var matchingVertices = new List<int>();
 		
 		for (var i = 0; i < meshVertices.Count; i++)
 		{
 			var vertex = meshVertices[i];
-			if (IsInsideFrustum(obj.transform.TransformPoint(vertex), planes) == inside)
+			if (IsInsideFrustum(objTransform.TransformPoint(vertex), planes) == inside)
 				matchingVertices.Add(i);
 		}
 		
 		return matchingVertices;
 	}
 
-	private List<int> GetVerticesOutsideViewFrustum(GameObject obj, in List<Vector3> meshVertices, Plane[] planes)
+	private List<int> GetVerticesOutsideViewFrustum(Transform gameObjectTransform, in List<Vector3> meshVertices, Plane[] planes)
 	{
-		return GetVerticesInRelationToViewFrustum(obj, meshVertices, planes, false);
+		return GetVerticesInRelationToViewFrustum(gameObjectTransform, meshVertices, planes, false);
 	}
 	
-	private List<int> GetVerticesInsideViewFrustum(GameObject obj, in List<Vector3> meshVertices, Plane[] planes)
+	private List<int> GetVerticesInsideViewFrustum(Transform gameObjectTransform, in List<Vector3> meshVertices, Plane[] planes)
 	{
-		return GetVerticesInRelationToViewFrustum(obj, meshVertices, planes, true);
+		return GetVerticesInRelationToViewFrustum(gameObjectTransform, meshVertices, planes, true);
 	}
 	private List<int> GetTrianglesInsideViewFrustrum(Transform gameObjectTransform, ref List<Vector3> meshVertices, ref List<int> matchingVertices, in int[] meshTriangles, in Plane[] planes)
 	{
@@ -180,20 +182,18 @@ public class Polaroid : MonoBehaviour
 		for (var i = 0; i < meshTriangles.Length; i += 3)
 		{
 			var contain = new List<bool> {false, false, false};
-			for (var j = 0; j < 3; j++)
-			{
+			
+			for(var j = 0; j < 3; j++)
 				contain[j] = matchingVertices.Contains(meshTriangles[i + j]);
-			}
 
-			var count = contain.Count(x => x);
+			var verticesInsideFrustumCount = contain.Count(x => x);
 			
 			// ReSharper disable once ConvertIfStatementToSwitchStatement
-			if (count == 3)
+			if (verticesInsideFrustumCount == 3)
 			{
 				newTriangles.AddRange(new[] {meshTriangles[i], meshTriangles[i + 1], meshTriangles[i + 2]});
 			}
-			
-			else if (count == 2)
+			else if (verticesInsideFrustumCount == 2)
 			{
 				var outsiderIndex = contain.IndexOf(false);
 				var insiderIndex = contain.IndexOf(true);
@@ -202,30 +202,35 @@ public class Polaroid : MonoBehaviour
 				var insider = meshVertices[meshTriangles[i + insiderIndex]];
 				var insider2 = meshVertices[meshTriangles[i + insiderIndex2]];
 				
+				//if (planes.Count(x => x.GetSide(gameObjectTransform.TransformPoint(outsider))) > 5) continue;
+				
 				{
 					var p = planes.Select(x => x.GetSide(outsider));
-					var c = planes.Count(x => x.GetSide(outsider));
-					var s = String.Join(", ", p);
-					Debug.Log($"{c} (" +
-					          $"{contain[outsiderIndex]}:{matchingVertices.Contains(meshTriangles[i + outsiderIndex])}, " +
-					          $"{contain[insiderIndex]}:{matchingVertices.Contains(meshTriangles[i + insiderIndex])}, " +
-					          $"{contain[insiderIndex2]}:{matchingVertices.Contains(meshTriangles[i + insiderIndex2])}) " +
-					          $"- {s}");
+					var c = (planes.Count(x => x.GetSide(outsider)), planes.Count(x => x.GetSide(insider)), planes.Count(x => x.GetSide(insider2)));
+					var s = string.Join(", ", p);
+//					Debug.Log($"{gameObjectTransform.gameObject.name} ({outsiderIndex}, {insiderIndex}, {insiderIndex2})[{c}] - (" +
+//					          $"{contain[outsiderIndex]}:{matchingVertices.Contains(meshTriangles[i + outsiderIndex])}, " +
+//					          $"{contain[insiderIndex]}:{matchingVertices.Contains(meshTriangles[i + insiderIndex])}, " +
+//					          $"{contain[insiderIndex2]}:{matchingVertices.Contains(meshTriangles[i + insiderIndex2])}) " +
+//					          $"- {s}");
 				}
-				
-				//if (planes.Count(x => !x.GetSide(outsider)) == 1)
+
+				var planesVertexIsOutsideOf =
+					planes.Where(x => !x.GetSide(gameObjectTransform.TransformPoint(outsider))).ToList();
+
+				var planesVertexIsOutsideOfCount = planesVertexIsOutsideOf.Count();
+				Debug.Log($"Outside {planesVertexIsOutsideOfCount}");
+				if (planesVertexIsOutsideOfCount == 1)
 				{
 					var insiders = new[] {insider, insider2};
 					var newPoints = new[] {outsider, outsider};
-					
-					foreach (var plane in planes)
+
+					foreach (var plane in planesVertexIsOutsideOf)
 					{
-						for (int p = 0; p < newPoints.Length; p++)
+						for (var p = 0; p < newPoints.Length; p++)
 						{
-							if (!plane.GetSide(gameObjectTransform.TransformPoint(newPoints[p])))
-							{
-								newPoints[p] = ProjectPointOnPlane(insiders[p],newPoints[p], plane, gameObjectTransform);
-							}
+							newPoints[p] = ProjectPointOnPlane(insiders[p], newPoints[p], plane,
+								gameObjectTransform);
 						}
 					}
 
@@ -236,11 +241,40 @@ public class Polaroid : MonoBehaviour
 					}
 
 					CreateTrapezoid(ref newTriangles, new[] {outsiderIndex, insiderIndex, insiderIndex2},
-						new[] { meshVertices.Count - 1, meshTriangles[i + insiderIndex], meshTriangles[i + insiderIndex2] },  
-						new[] { meshVertices.Count - 2, meshTriangles[i + insiderIndex], meshVertices.Count - 1 });
+						new[] {meshVertices.Count - 1, meshTriangles[i + insiderIndex], meshTriangles[i + insiderIndex2]},
+						new[] {meshVertices.Count - 2, meshTriangles[i + insiderIndex], meshVertices.Count - 1});
+				}
+				else if (planesVertexIsOutsideOfCount == 2)
+				{
+					var insiders = new[] {insider, insider2};
+					var newPoints = new[] {outsider, outsider, outsider};
+					
+					foreach (var plane in planesVertexIsOutsideOf)
+					{
+						for (var p = 0; p < 2; p++)
+						{
+							newPoints[p] = ProjectPointOnPlane(insiders[p], newPoints[p], plane,
+								gameObjectTransform);
+						}
+						
+						newPoints[2] = ProjectPointOnPlane(insiders[0], newPoints[2], plane, gameObjectTransform);
+						newPoints[2] = ProjectPointOnPlane(insiders[1], newPoints[2], plane, gameObjectTransform);
+					}
+
+					foreach (var p in newPoints)
+					{
+						meshVertices.Add(p);
+						matchingVertices.Add(meshVertices.Count - 1);
+					}
+
+//					CreateTrapezoid(ref newTriangles, new[] {outsiderIndex, insiderIndex, insiderIndex2},
+//						new[]{meshVertices.Count - 1, meshTriangles[i + insiderIndex], meshTriangles[i + insiderIndex2]},
+//						new[] {meshVertices.Count - 2, meshTriangles[i + insiderIndex], meshVertices.Count - 1});
+					
+//					newTriangles.AddRange(new[] {meshVertices.Count - 3, meshVertices.Count - 2, meshVertices.Count - 1});
 				}
 			}
-			else if (count == 1)
+			else if (verticesInsideFrustumCount == 1)
 			{
 				var insiderIndex = contain.IndexOf(true);
 				var outsiderIndex = contain.IndexOf(false);
@@ -251,15 +285,50 @@ public class Polaroid : MonoBehaviour
 				var outsider2 = meshVertices[meshTriangles[i + outsiderIndex2]];
 				
 				var outsiders = new []{outsider, outsider2};
-				
-				foreach (var plane in planes)
+				for (var p = 0; p < outsiders.Length; p++)
 				{
-					for (var p = 0; p < outsiders.Length; p++)
+					var planesVertexIsOutsideOf =
+						planes.Where(x => !x.GetSide(gameObjectTransform.TransformPoint(outsiders[p]))).ToList();
+
+					var planesVertexIsOutsideOfCount = planesVertexIsOutsideOf.Count();
+					
+					projectedPointsToDraw1.Add(gameObjectTransform.TransformPoint(insider));
+					projectedPointsToDraw1.Add(gameObjectTransform.TransformPoint(outsider));
+					projectedPointsToDraw1.Add(gameObjectTransform.TransformPoint(outsider2));
+
+					if (planesVertexIsOutsideOfCount > 1)
 					{
-						if (!plane.GetSide(gameObjectTransform.TransformPoint(outsiders[p])))
+						var plane1 = planesVertexIsOutsideOf[0];
+						var plane2 = planesVertexIsOutsideOf[1];
+						var plane3 = new Plane();
+						var o = GetClockwiseRotation(new[] {insiderIndex, outsiderIndex, outsiderIndex2}, new[]
 						{
-							outsiders[p] = ProjectPointOnPlane(insider, outsiders[p], plane, gameObjectTransform);
-						}
+							meshTriangles[i + insiderIndex],
+							meshTriangles[i + outsiderIndex],
+							meshTriangles[i + outsiderIndex2]
+						});
+						plane3.Set3Points
+						(
+						gameObjectTransform.TransformPoint(meshVertices[o[0]]),
+						gameObjectTransform.TransformPoint(meshVertices[o[1]]),
+						gameObjectTransform.TransformPoint(meshVertices[o[2]])
+						);
+
+//							projectedPointsToDraw1.Add(gameObjectTransform.TransformPoint(meshVertices[o[0]]));
+//							projectedPointsToDraw1.Add(gameObjectTransform.TransformPoint(meshVertices[o[1]]));
+//							projectedPointsToDraw1.Add(gameObjectTransform.TransformPoint(meshVertices[o[2]]));
+//						
+						if (!MathExtensions.PlanesIntersectAtSinglePoint(plane1, plane3, plane2, out outsiders[p]))
+							MathExtensions.PlanesIntersectAtSinglePoint(plane1, plane2, plane3, out outsiders[p]);
+						
+//						projectedPointsToDraw3.Add(outsiders[p]);
+						outsiders[p] = gameObjectTransform.InverseTransformPoint(outsiders[p]);
+					}
+					
+					else
+					{
+						var plane = planesVertexIsOutsideOf[0];
+						outsiders[p] = ProjectPointOnPlane(insider, outsiders[p], plane, gameObjectTransform);
 					}
 				}
 				
@@ -339,6 +408,9 @@ public class Polaroid : MonoBehaviour
 		newMesh.SetVertices(newVertices);
 		newMesh.SetTriangles(newTriangles.ToArray(), 0);
 		newMesh.RecalculateNormals();
+		newMesh.RecalculateBounds();
+		newMesh.RecalculateTangents();
+
 		newObject.GetComponent<MeshFilter>().mesh = newMesh;
 
 		var col = newObject.GetComponent<MeshCollider>();
